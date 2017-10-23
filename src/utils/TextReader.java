1ccd7978e7c34086b100ecfbbf4626a77b3b7a00
nicocurat <nicurat@gmail.com>
2017-10-22 21:55:52 -0300

    100% working with tags and all

--- src/utils/TextReader.java
+++ src/utils/TextReader.java
@@ -35,9 +35,20 @@ public class TextReader {
             FileReader fileReader = new FileReader(htmlFile);
             int a = fileReader.read();
             while (a != -1) {
-                if(a == 60){ //Condicion para fijarse que no sea tag.
+                //Condicion para fijarse que no sea tag.
+                if(a == 60){
+                    //Si empieza un tag, guardo todas los caracteres en una lista auxiliar
+                    //hasta que sepa que si hay un closing tag los borro,
+                    //pero si hay un open tag antes entonces significa que es texto y
+                    //agrego la lista aux en la lista de letras!
+                    List<Character> aux = new ArrayList<>();
+                    aux.add((char) a);
                     a = fileReader.read();
-                    while (a != 62){
+                    while (a != 62){ //Mientras que no sea un tag!
+                        if(a == 60){
+                            letters.addAll(aux);
+                            aux = new ArrayList<>();
+                        } else aux.add((char) a);
                         a = fileReader.read();
                     }
                     a = fileReader.read();
